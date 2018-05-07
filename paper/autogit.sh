d31b7d30670535d67862d37a1158483edadf37a2
Seth Russell <seth.russell@ucdenver.edu>
2018-05-07 11:45:09 -0600

    Auto Update from local Sharelatex

---
+++ paper/autogit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /home/seth/faker-prototype/paper
+cp /home/seth/sharelatex/sharelatex_data/data/compiles/5aecc5ba3a518f007ab52133-5aeb8780c8a91e016452182f/*.{tex,bib,sty} ./
+git pull
+git add *
+git commit -a -m 'Auto Update from local Sharelatex'
+git push
