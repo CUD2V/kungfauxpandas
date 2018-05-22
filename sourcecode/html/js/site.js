1db3f078106f2e7f873dc19bdfd52e5812091cba
Seth Russell <magic-lantern@users.noreply.github.com>
2018-05-21 20:35:49 -0600

    Saving work for today.

--- sourcecode/html/js/site.js
+++ sourcecode/html/js/site.js
@@ -5,35 +5,30 @@ Custom javascript stuff goes here
 //Need to configure port for AJAX calls. Default is port 8000 on same host as html
 var HOST = window.location.hostname;
 var PORT = '8000';
+var DEBUG_MODE = true;
 
 $('#query_results').on('hidden.bs.collapse', function () {
-  console.log('caught hidden event');
   $('#qr_indicator').removeClass('fa-minus');
   $('#qr_indicator').addClass('fa-plus');
 })
 
 $('#query_results').on('shown.bs.collapse', function () {
-  console.log('caught shown event');
   $('#qr_indicator').removeClass('fa-plus');
   $('#qr_indicator').addClass('fa-minus');
 })
 
 $('#metadata').on('hidden.bs.collapse', function () {
-  console.log('caught hidden event');
   $('#m_indicator').removeClass('fa-minus');
   $('#m_indicator').addClass('fa-plus');
 })
 
 $('#metadata').on('shown.bs.collapse', function () {
-  console.log('caught shown event');
   $('#m_indicator').removeClass('fa-plus');
   $('#m_indicator').addClass('fa-minus');
 })
 
 $('#submit').click(function() {
-  console.log('Called submit click.')
-
-  var jqxhr = $.ajax({
+  var jqxhr_query = $.ajax({
     dataType: 'json',
     type: 'GET',
     url: 'http://' + HOST + ':' + PORT + '/synthesize_data',
@@ -46,11 +41,27 @@ $('#submit').click(function() {
       // need to add elements to query result area...
       // create columns
       // populate rows
-      console.log(jqxhr);
-      console.log(data);
+      if(DEBUG_MODE) {
+        console.log(jqxhr_query);
+        console.log(data);
+      }
+      if (data.message === 'success'){
+        $('#qr_indicator').parent().removeClass('alert-danger');
+        $('#query_result_text').html('<pre>' + data.response + '</pre>');
+      }
+      else{
+        $('#qr_indicator').parent().addClass('alert-danger');
+        var response = '<p>Error encountered while attempting to get synthetic data. See browser console for additional details.</p>';
+        response += '<p>Query submitted:</p>';
+        response += '<pre>' + data.query + '</pre>';
+        $('#query_result_text').html(response);
+        console.log(data.response);
+      }
     })
     .fail(function(jqXHR, textStatus, errorThrown) {
-      console.log('error encountered attempting to get synthetic data');
+      console.log('error encountered attempting to get synthetic data' + textStatus + errorThrown);
+      $('#qr_indicator').parent().addClass('alert-danger');
+      $('#query_result_text').html('Error encountered while attempting to get synthetic data. See browser console for additional details.');
     });
 
 })
@@ -60,6 +71,8 @@ $('#reset').click(function() {
   editor.focus(); //To focus the ace editor
   var n = editor.getSession().getValue().split('\n').length; // To count total no. of lines
   editor.gotoLine(n); //Go to end of document
+  $('#qr_indicator').parent().removeClass('alert-danger');
+  $('#query_result_text').html('Type a query and hit submit to see results here.');
 })
 
 var editor = ace.edit('editor');
@@ -72,30 +85,57 @@ editor.setOptions({
 });
 $('#reset').click();
 
-// Assign handlers immediately after making the request,
-// and remember the jqXHR object for this request
-var jqxhr = $.ajax({
-  dataType: 'json',
-  url: 'http://' + HOST + ':' + PORT + '/synthesis_methods'})
-  .done(function(data) {
-    // need to add elements to UI...
-    console.log(jqxhr);
-    console.log(data);
-    if (data.response.constructor === Array) {
-      data.response.forEach(function(m, index){
-        console.log(m);
-        var html_to_append = '<div class="form-check form-check-inline"><input class="form-check-input" type="radio" name="selected_method" id="'
-        html_to_append += 'selected_method' + index + '"';
-        if (m === 'kde')
-          html_to_append += 'value="' + m + '" checked>';
-        else {
-          html_to_append += 'value="' + m + '">';
-        }
-        html_to_append += '<label class="form-check-label" for="inlineRadio1">' + m + '</label></div>';
-        $('#synthesis_methods').append(html_to_append);
-      });
-    }
-  })
-  .fail(function(jqXHR, textStatus, errorThrown) {
-    console.log('error encountered attempting to get data synthesis methods');
-  });
+$( document ).ready(function() {
+  // Get Synthesis Methods
+  var jqxhr_synthesis = $.ajax({
+    dataType: 'json',
+    url: 'http://' + HOST + ':' + PORT + '/synthesis_methods'})
+    .done(function(data) {
+      if(DEBUG_MODE) {
+        console.log(jqxhr_synthesis);
+        console.log(data);
+      }
+      if (data.response.constructor === Array) {
+        data.response.forEach(function(m, index){
+          if(DEBUG_MODE)
+            console.log(m);
+          var html_to_append = '<div class="form-check form-check-inline"><input class="form-check-input" type="radio" name="selected_method" id="'
+          html_to_append += 'selected_method' + index + '"';
+          if (m === 'kde')
+            html_to_append += 'value="' + m + '" checked>';
+          else {
+            html_to_append += 'value="' + m + '">';
+          }
+          html_to_append += '<label class="form-check-label" for="inlineRadio1">' + m + '</label></div>';
+          $('#synthesis_methods').append(html_to_append);
+        });
+      }
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      console.log('error encountered attempting to get data synthesis methods');
+      $('#errorModal').modal('show');
+    });
+
+  // populate Database Metadata field
+  // Get Synthesis Methods
+  var jqxhr_metadata = $.ajax({
+    dataType: 'json',
+    url: 'http://' + HOST + ':' + PORT + '/metadata'})
+    .done(function(data) {
+      if(DEBUG_MODE) {
+        console.log(jqxhr_metadata);
+        console.log(data);
+      }
+      if (data.response.constructor === Array) {
+        data.response.forEach(function(r, index){
+          if (DEBUG_MODE)
+            console.log(r);
+          $('#metadata_text').append('<pre>' + r + '</pre>');
+        });
+      }
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      console.log('error encountered attempting to get metadata');
+      $('#errorModal').modal('show');
+    });
+});
--- sourcecode/python/web-service.py
+++ sourcecode/python/web-service.py
@@ -1,10 +1,13 @@
 import hug
-from fakelite3 import KungFauxPandas
+from seth_fakelite3 import KungFauxPandas
 import sqlite3
+import sys
+
 
 kfpd = KungFauxPandas()
-dbname = '../../data/sepsis.db'
-db_conn = sqlite3.connect(dbname)
+dbname = 'file:../../data/sepsis.db?mode=ro'
+db_conn = sqlite3.connect(dbname, uri=True)
+cursor = db_conn.cursor()
 
 @hug.response_middleware()
 def process_data(request, response, resource):
@@ -13,13 +16,23 @@ def process_data(request, response, resource):
 @hug.get(examples='query=select * from table&method=kde')
 @hug.local()
 def synthesize_data(query: hug.types.text, method: hug.types.text):
-    """Does nothing right now"""
-    response = 'No data found'
-    df = kfpd.read_sql(query, db_conn, method)
-    return {
-        'message': 'need to provide message here',
-        'query': '{0}'.format(query),
-        'response': df.to_json()}
+    try:
+        print('line 20')
+        df = kfpd.read_sql(query, db_conn, method)
+        print('line 22')
+        print(type(df))
+        print(df)
+        df.style.hide_index()
+        return {
+          'message': 'success',
+          'query': '{0}'.format(query),
+          'response': df.to_html(classes='table')}
+    except Exception as e:
+        print('Caught exception', str(e))
+        return {
+            'message': 'error',
+            'query': '{0}'.format(query),
+            'response': str(e)}
 
 @hug.get()
 @hug.local()
@@ -27,3 +40,17 @@ def synthesis_methods():
     return {
         'message': 'available data synthesis methods',
         'response': kfpd.synthesis_methods}
+
+@hug.get()
+@hug.local()
+def metadata():
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+    response = []
+    for c in tables:
+        cursor.execute("SELECT sql FROM sqlite_master WHERE name='" + c[0] + "'")
+        response.append(cursor.fetchall()[0][0])
+
+    return {
+        'message': 'success',
+        'response': response}
