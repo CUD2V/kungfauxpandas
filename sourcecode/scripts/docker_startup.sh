6733fb6aa1cf61c48a5916269522781d06b9aff7
Seth Russell <magic-lantern@users.noreply.github.com>
2018-05-25 14:33:53 -0600

    more tweaks for docker

--- sourcecode/scripts/docker_startup.sh
+++ sourcecode/scripts/docker_startup.sh
@@ -3,7 +3,7 @@ cd ../html
 python -m http.server 8080 >> /app/http_server.log 2>&1
 
 cd ../python
-hug -p 8000 -f web-service.py >> /app/web_service.log 2>&1
+/usr/local/bin/hug -p 8000 -f web-service.py >> /app/web_service.log 2>&1
 
 #tail -f /app/web_service.log
 
