13c0f82ca3e3aed8a84fb243947449fe9022b84a
Seth Russell <magic-lantern@users.noreply.github.com>
2018-05-25 14:18:53 -0600

    updates...

--- sourcecode/scripts/docker_startup.sh
+++ sourcecode/scripts/docker_startup.sh
@@ -3,6 +3,11 @@ cd ../html
 python -m http.server 8080 >> /app/http_server.log 2>&1
 
 cd ../python
-hug -p 8000 -f web-service.py >> /app/web_service.log 2>&1 
+hug -p 8000 -f web-service.py >> /app/web_service.log 2>&1
 
-tail -f /app/web_service.log
+#tail -f /app/web_service.log
+
+while :; do
+  sleep 100
+  echo -n "."
+done
