b36a50284da2386ad90d5f99c692cc3fbeb58e91
Seth Russell <magic-lantern@users.noreply.github.com>
2018-05-26 15:39:33 -0600

    Forgot to fix path to hug

--- sourcecode/scripts/docker_startup.sh
+++ sourcecode/scripts/docker_startup.sh
@@ -7,7 +7,7 @@ cd ../html
 python -m http.server 8080 >> /app/http_server.log 2>&1
 
 cd ../python
-/usr/local/bin/hug -p 8000 -f web-service.py >> /app/web_service.log 2>&1
+hug -p 8000 -f web-service.py >> /app/web_service.log 2>&1
 
 #tail -f /app/web_service.log
 
