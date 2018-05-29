e931b371598d48a0826c38ed91aca40efb6e560c
magic-lantern <magic-lantern@users.noreply.github.com>
2018-05-29 09:12:09 -0600

    Auto Update from local Sharelatex

--- paper/autogit.sh
+++ paper/autogit.sh
@@ -3,5 +3,5 @@ cd /home/seth/kungfauxpandas/paper
 cp /home/seth/sharelatex/sharelatex_data/data/compiles/5aecc5ba3a518f007ab52133-5aeb8780c8a91e016452182f/*.{tex,bib,sty} ./
 git pull
 git add *
-git commit -a -m 'Auto Update from local Sharelatex'
+git commit -a -m 'Auto Update from local Sharelatex'  --author="magic-lantern <magic-lantern@users.noreply.github.com>"
 git push
