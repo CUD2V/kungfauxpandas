21bf4b2f756a262e2821595f9e70520aca1f9fd2
magic-lantern <magic-lantern@users.noreply.github.com>
2018-06-02 20:27:46 -0600

    Changing order of operations so that web based edits are preserved if change occurs elsewhere.

--- paper/autogit.sh
+++ paper/autogit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /home/seth/kungfauxpandas/paper
+git pull
 cp /home/seth/sharelatex/sharelatex_data/data/compiles/5aecc5ba3a518f007ab52133-5aeb8780c8a91e016452182f/*.{tex,bib,sty} ./
 cp -rp /home/seth/sharelatex/sharelatex_data/data/compiles/5aecc5ba3a518f007ab52133-5aeb8780c8a91e016452182f/images ./
-git pull
 git add *
 git commit -a -m 'Auto Update from local Sharelatex'  --author="magic-lantern <magic-lantern@users.noreply.github.com>"
 git push
