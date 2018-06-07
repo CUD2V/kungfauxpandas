9edbaeef2a95b77545c50b8de69897cc97b95587
magic-lantern <magic-lantern@users.noreply.github.com>
2018-06-07 13:15:46 -0600

    Fixing autogit so it only commits changes in the paper directory.

--- paper/autogit.sh
+++ paper/autogit.sh
@@ -4,5 +4,5 @@ git pull
 cp /home/seth/sharelatex/sharelatex_data/data/compiles/5aecc5ba3a518f007ab52133-5aeb8780c8a91e016452182f/*.{tex,bib,sty} ./
 cp -rp /home/seth/sharelatex/sharelatex_data/data/compiles/5aecc5ba3a518f007ab52133-5aeb8780c8a91e016452182f/images ./
 git add *
-git commit -a -m 'Auto Update from local Sharelatex'  --author="magic-lantern <magic-lantern@users.noreply.github.com>"
+git commit -m 'Auto Update from local Sharelatex'  --author="magic-lantern <magic-lantern@users.noreply.github.com>"
 git push
